facb60f94e5af2205705c37c2e7840c7dfe88d11
davmoon <brightsun2001@gmail.com>
2024-08-02 15:37:32 +0900

    main.js fix

--- MDN/js/main.js
+++ MDN/js/main.js
@@ -7,29 +7,34 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-var storyText = [`It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.`];
-var insertX = [`Willy the Goblin
-Big Daddy
-Father Christmas`];
-var insertY = [`the soup kitchen
-Disneyland
-the White House`];
-var insertZ = [`spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away`];
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const insertX = ['Willy the Goblin', 
+'Big Daddy',
+'Father Christmas'];
+const insertY = ['the soup kitchen',
+'Disneyland',
+'the White House'];
+const insertZ = ['spontaneously combusted',
+'melted into a puddle on the sidewalk',
+'turned into a slug and crawled away'];
 
 randomize.addEventListener('click', result);
 
 function result() {
 
-    var newStory = storyText;
-    var xItem;
-    var yItem;
-    var zItem;
+    let newStory = storyText;
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+
+    newStory = newStory.replaceAll(":insertx:", xItem);
+    newStory = newStory.replaceAll(":inserty:", yItem);
+    newStory = newStory.replaceAll(":insertz:", zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace("Bob", name);
+    newStory = newStory.replaceAll("Bob", name);
+    
   }
 
   if(document.getElementById("uk").checked) {
@@ -38,6 +43,6 @@ function result() {
 
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
